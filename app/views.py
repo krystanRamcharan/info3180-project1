1c82039fe256502dc6f1c6551f10ac4d030711b0
krystanRamcharan <44042620+krystanRamcharan@users.noreply.github.com>
2020-03-14 15:06:02 -0400

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,2 +1,52 @@
 from app import app
 from flask import render_template,request,,redirect,url_for,flash
+
+
+
+
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+  
+  
+@app.route('/')
+def home():
+    """Render website's home page."""
+    return render_template('home.html')
+  
+  
+  
+@app.route('/about/')
+def about():
+    """Render the website's about page."""
+    return render_template('about.html', name="Krystan Ramcharan ")
+  
+@app.route('/<file_name>.txt')
+def send_text_file(file_name):
+   file_dot_text = file_name + '.txt'
+    return app.send_static_file(file_dot_text)
+  
+  
+ 
+@app.after_request
+def add_header(response):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also tell the browser not to cache the rendered page. If we wanted
+    to we could change max-age to 600 seconds which would be 10 minutes.
+    """
+    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    response.headers['Cache-Control'] = 'public, max-age=0'
+    return response
+  
+  
+  
+@app.errorhandler(404)
+def page_not_found(error):
+    """Custom 404 page."""
+    return render_template('404.html'), 404
+  
+  
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port="8080")
