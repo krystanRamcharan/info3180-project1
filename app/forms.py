3fd06068f5a5a82a4686ac5a7bb7f1d5f4152695
krystanRamcharan <44042620+krystanRamcharan@users.noreply.github.com>
2020-04-15 22:24:36 -0400

    Create forms.py

---
+++ app/forms.py
@@ -0,0 +1,20 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from wtforms import StringField
+from wtforms.validators import DataRequired, Email
+
+
+class MyForm(FlaskForm):
+    firstname = StringField('First Name', validators=[DataRequired()])
+    lastname = StringField('Last Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    location =StringField('location'), validators=[DataRequired()])
+    Gender= RadioField('Gender', choices=[('1','Female'),('2','Male')])
+
+
+class PhotoForm(FlaskForm):
+    photo = FileField('Photo', validators=[
+        FileRequired(),
+        FileAllowed(['jpg', 'png', 'Images only!'])
+    ])
+    description = StringField('Description', validators=[DataRequired()])
